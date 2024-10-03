83871f39b916324981e198b68055e142ea07c4b6
1104sona panda <sonali.pnd04@gmail.com>
2024-10-03 08:49:53 +0000

    last commit

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
-const express = require('express');        // Import Express
-const mongoose = require('mongoose');      // Import Mongoose
-const userRoutes = require('./routes/userRoutes');  // Import book routes
+const express = require("express")
+const connectDB = require("./config/db")
+const cors = require("cors")
+const books = require("./routes/api/books")
+const path = require('path');
+require("dotenv").config( { path: "./config.env" } )
 
-// Initialize the Express app
-const app = express();
+// CONNECT TO DB
+connectDB()
 
-// Middleware to parse incoming JSON requests
+// INITIATE APP
+const app = express()
+
+
+
+// HANDLE MIDDLEWARE
 app.use(express.json());
-
-// Use the book routes with a base URL of '/api'
-app.use('/api', userRoutes);
-
-connectDB();
+app.use(cors());
+app.use("/api/Books", Books)
 
 
-// Setup a default route for checking server status
-app.get('/', (req, res) => {
-    res.send('Library Management System API is running');
+// SERVE STATIC FILES
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+    res.sendFile(
+        path.join(__dirname, "./client/build/index.html"),
+        function (err) {
+            res.status(500).send(err);
+        }
+    );
 });
 
-// Define the port where the server will listen
-const PORT = process.env.PORT || 3000;
 
-// Start the server and listen on the specified port
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// START SERVER
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
